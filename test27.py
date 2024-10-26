583a2fe9571da34fdc416c6dc07ab007da269f11
IgnacioPaniagua <119708571+IgnacioPaniagua@users.noreply.github.com>
2024-10-25 21:58:53 -0300

    Update test27.py

--- test27.py
+++ test27.py
@@ -13,14 +13,14 @@ def get_specific_volumes(pressure):
         specific_volume_liquid = 0.00105
         specific_volume_vapor = 30  # Valor representativo
     elif 0.5 <= pressure < critical_pressure:  # Línea de líquido saturado
-        specific_volume_liquid = 0.00105 + (critical_volume_liquid - 0.00105) * ((pressure - 0.5) / (9.5))
-        specific_volume_vapor = critical_volume_vapor - (critical_volume_vapor - specific_volume_liquid) * ((pressure - 0.5) / (9.5))
+        specific_volume_liquid = 0.00105 + (critical_volume_liquid - 0.00105) * ((pressure - 0.5) / (critical_pressure - 0.5))
+        specific_volume_vapor = 30 - (30 - critical_volume_vapor) * ((pressure - 0.5) / (critical_pressure - 0.5))
     elif pressure == critical_pressure:  # En el punto crítico
         specific_volume_liquid = critical_volume_liquid
         specific_volume_vapor = critical_volume_vapor
     elif critical_pressure < pressure <= 30:  # Línea de vapor saturado
         specific_volume_liquid = critical_volume_liquid  # Se mantiene constante
-        specific_volume_vapor = critical_volume_vapor - (pressure - critical_pressure) * (critical_volume_vapor - 0.05) / (30 - critical_pressure)
+        specific_volume_vapor = critical_volume_vapor - (critical_volume_vapor - 0.05) * ((pressure - critical_pressure) / (30 - critical_pressure))
     else:  # Fuera de los límites
         return None, None  # Manejo de error
 
